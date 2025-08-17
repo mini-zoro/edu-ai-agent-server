b41fb92e96dbbd1f0dae9ffd7ba5fe313964f7ed
Guqier <2535810484@qq.com>
2025-08-17 21:59:31 +0800

    feat: 优化权限模型.

--- apps/permission/models.py
+++ apps/permission/models.py
@@ -8,7 +8,15 @@ from apps.auth.models import User
 
 class Permission(BaseModel):
     id = models.BigIntegerField(primary_key=True, db_comment='权限ID')
-    parent_id = models.BigIntegerField(blank=True, null=True, db_comment='父级权限ID')
+    parent = models.ForeignKey(
+        'self',  # 自引用外键
+        on_delete=models.RESTRICT,
+        blank=True,
+        null=True,
+        db_column='parent_id',  # 数据库列名仍叫 parent_id
+        related_name='children',  # 反向查询：perm.children.all()
+        db_comment='父级权限'
+    )
     key = models.CharField(max_length=255, blank=True, null=True, db_comment='权限码（权限路径）')
     type = models.IntegerField(blank=True, null=True, db_comment='权限类型，0表示页面权限，1表示操作权限')
     name = models.CharField(max_length=255, blank=True, null=True, db_comment='权限名')
