3718b74f6b3e3ffdafc627206e0109ad38f7ae7b
Guqier <2535810484@qq.com>
2025-08-26 01:06:41 +0800

    feat(权限管理): 添加获取权限树的接口到URL路由.

--- apps/permission/urls.py
+++ apps/permission/urls.py
@@ -15,4 +15,5 @@ import apps.permission.views
 urlpatterns = [
     path('', apps.permission.views.PermissionView.as_view()),
     path('/roleId/<int:role_id>', apps.permission.views.get_permission_by_role),
+    path('/tree', apps.permission.views.get_permission_tree)
 ]
